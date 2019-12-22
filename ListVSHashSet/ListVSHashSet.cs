f8575a7ef71fde1b534b1eb684bd9b6111854e6a
isaev-bolsen <al_mamistov@mail.ru>
2019-12-22 09:49:13 +0300

    ListVSHashSet

---
+++ ListVSHashSet/ListVSHashSet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ListVSHashSet
+{
+	[TestClass]
+	public class ListVSHashSet
+	{
+		private const int elementsCount = int.MaxValue / 100;
+
+		[TestMethod]
+		public void AddItemHashSet()
+		{
+			HashSet<string> col = new HashSet<string>();
+			for (int i = 0; i < elementsCount; ++i)
+			{
+				col.Add(i.ToString());
+			}
+		}
+
+		[TestMethod]
+		public void AddItemList()
+		{
+			List<string> col = new List<string>();
+			for (int i = 0; i < elementsCount; ++i)
+			{
+				col.Add(i.ToString());
+			}
+		}
+
+		[TestMethod]
+		public void AddUniqueItemList()
+		{
+			List<string> col = new List<string>();
+			for (int i = 0; i < elementsCount; ++i)
+			{
+				string str = i.ToString();
+				if (!col.Contains(str))
+					col.Add(str);
+			}
+		}
+	}
+}
